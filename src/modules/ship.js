6e918b08aaf207f54c96ad49452480c9dbdb7811
cel8 <alex.celotti@gmail.com>
2023-02-13 13:25:47 -0500

    Ship class testing.

---
+++ src/modules/ship.js
@@ -0,0 +1,17 @@
+export default class Ship {
+  constructor(length) {
+    if (Number.isNaN(+length)) throw new 'Invalid input';
+    if (+length < 2 || +length > 5) throw new 'Invalid out-of-range';
+
+    this.length = +length;
+    this.hits = 0;
+  }
+
+  hit() {
+    this.hits += 1;
+  }
+
+  isSunk() {
+    return this.hits === this.length;
+  }
+};
---
+++ test/modules/ship.test.js
@@ -0,0 +1,48 @@
+// import Ship from 'Modules/ship';
+import Ship from '../../src/modules/ship';
+
+it('expect invalid ship length #undefined', () => {
+  expect(() => new Ship()).toThrow('Invalid input');
+});
+
+it('expect invalid ship length #string', () => {
+  expect(() => new Ship('ciao')).toThrow('Invalid input');
+});
+
+it('expect invalid ship length #out-of-range', () => {
+  expect(() => new Ship('6')).toThrow('Invalid out-of-range');
+  expect(() => new Ship('1')).toThrow('Invalid out-of-range');
+});
+
+it('valid object #ship length 2', () => {
+  expect(() => expect(new Ship(2)).toBeDefined()).not.toThrow('Invalid input');
+});
+
+it('valid ship object', () => {
+  const ship = new Ship(3);
+  expect(ship.length).toBeDefined();
+  expect(ship.hits).toBeDefined();
+});
+
+it('hit a ship', () => {
+  const ship = new Ship(3);
+  expect(ship.hits).toBe(0);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+});
+
+it('Ship is not sunk', () => {
+  const ship = new Ship(3);
+  expect(ship.isSunk()).toBeFalsy();
+  ship.hit();
+  expect(ship.isSunk()).toBeFalsy();
+});
+
+it('Ship is now sunk', () => {
+  const ship = new Ship(3);
+  expect(ship.isSunk()).toBeFalsy();
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBeTruthy();
+});

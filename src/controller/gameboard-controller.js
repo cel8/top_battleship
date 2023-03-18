e8324c6a1bbd6d3a9cf194a19566589b53652327
cel8 <alex.celotti@gmail.com>
2023-03-18 13:14:27 -0400

    Complete gameboard and its tests.

--- src/controller/gameboard-controller.js
+++ src/controller/gameboard-controller.js
@@ -9,6 +9,7 @@ export default class GameboardController {
     for (let i = 0; i < BOARD_SIZE; i += 1) this.gameboard[i] = new Array(BOARD_SIZE);
     this.userGameboard = userGameboard || false;
     this.trackAttacks = new AttackTrackerSet();
+    this.shipMap = new Map();
   }
 
   static #isValidCoord(x, y) {
@@ -21,28 +22,53 @@ export default class GameboardController {
   isUserGameboard() { return this.userGameboard; }
 
   place(ship, x, y, vertical = false) {
+    const coordinates = [];
     if (!(ship instanceof Ship)) return false;
     if (!GameboardController.#isValidCoord(x, y) ||
        (vertical  && (+x + ship.length >= BOARD_SIZE)) ||
        (!vertical && (+y + ship.length >= BOARD_SIZE))) return false;
 
     if (vertical) {
-      for (let i = +x; i < (+x + ship.length); i += 1) this.gameboard[i][y] = ship;
+      for (let i = +x; i < (+x + ship.length); i += 1) { 
+        coordinates.push({ x: i, y: +y });
+        this.gameboard[i][+y] = ship;
+      }
     } else {
-      for (let i = +y; i < (+y + ship.length); i += 1) this.gameboard[x][i] = ship;
+      for (let i = +y; i < (+y + ship.length); i += 1) {
+        coordinates.push({ x: +x, y: i })
+        this.gameboard[+x][i] = ship;
+      }
     }
     
+    // Add ship into ship container
+    this.shipMap.set(ship, coordinates);
+
     return true;
   }
 
   receiveAttack(x, y) {
-    if (!GameboardController.#isValidCoord(x, y)) return false;
-    if (this.trackAttacks.has({x: +x, y: +y})) return false;
+    if (!GameboardController.#isValidCoord(x, y)) return { exit: false };
+    if (this.trackAttacks.has({x: +x, y: +y})) return { exit: false };
     this.trackAttacks.add({x: +x, y: +y});
     if (this.gameboard[+x][+y] instanceof Ship) {
       this.gameboard[+x][+y].hit();
-      return true;
+      if (this.gameboard[+x][+y].isSunk()) {
+        return {
+          exit: true,
+          water: false,
+          sunk: true,
+          coordinates: this.shipMap.get(this.gameboard[+x][+y])
+        }; 
+      }
+      return {
+        exit: true,
+        water: false,
+        sunk: false
+      };
     }
-    return false;
+    return {
+      exit: true,
+      water: true
+    };
   }
 };
--- test/controller/gameboard-controller.test.js
+++ test/controller/gameboard-controller.test.js
@@ -4,12 +4,35 @@ import GameboardController from 'Controller/gameboard-controller';
 let gameboardController;
 let patrolBoat;
 
+const invalidAttack = {
+  exit: false
+};
+
+const validAttackWater = {
+  exit: true,
+  water: true
+};
+
+const validAttackHit = {
+  exit: true,
+  water: false,
+  sunk: false
+};
+
+const validAttackSunk = {
+  exit: true,
+  water: false,
+  sunk: true,
+  coordinates: [ {x: 0, y: 0 },  {x: 0, y: 1 } ]
+};
+
 beforeEach(() => {
   gameboardController = new GameboardController();
   patrolBoat = new Ship(2);
   // set a ship in [0,0] vertical
   gameboardController.gameboard[0][0] = patrolBoat;
   gameboardController.gameboard[0][1] = patrolBoat;
+  gameboardController.shipMap.set(patrolBoat, [ {x: 0, y: 0 }, { x: 0, y: 1 }]);
 });
 
 it('create gameboard', () => {
@@ -28,22 +51,26 @@ it('place a 2 length (horizontal) ship in valid coordinate', () => {
   expect(gameboardController.place(patrolBoat, 3, 5)).toBeTruthy();
   expect(gameboardController.gameboard[3][5]).toBe(patrolBoat);
   expect(gameboardController.gameboard[3][6]).toBe(patrolBoat);
+  expect(gameboardController.shipMap.get(patrolBoat)).toEqual([ {x:3,y:5}, {x:3,y:6} ]);
 });
 
 it('place a 2 length (vertical) ship in valid coordinate', () => {
   expect(gameboardController.place(patrolBoat, 3, 5, true)).toBeTruthy();
   expect(gameboardController.gameboard[3][5]).toBe(patrolBoat);
   expect(gameboardController.gameboard[4][5]).toBe(patrolBoat);
+  expect(gameboardController.shipMap.get(patrolBoat)).toEqual([ {x:3,y:5}, {x:4,y:5} ]);
 });
 
 it('place a 2 length (horizontal) ship in invalid coordinate', () => {
   expect(gameboardController.place(patrolBoat, 3, 9)).toBeFalsy();
   expect(gameboardController.gameboard[3][9]).toBeUndefined();
+  expect(gameboardController.shipMap.get(patrolBoat)).not.toEqual([ {x:3,y:9} ]);
 });
 
 it('place a 2 length (vertical) ship in invalid coordinate', () => {
   expect(gameboardController.place(patrolBoat, 9, 5, true)).toBeFalsy();
   expect(gameboardController.gameboard[9][5]).toBeUndefined();
+  expect(gameboardController.shipMap.get(patrolBoat)).not.toEqual([ {x:9,y:5} ]);
 });
 
 it('invalid place location #1', () => {
@@ -64,28 +91,47 @@ it('invalid place location #4', () => {
 
 it('receive an attack', () => {
   expect(gameboardController.trackAttacks.has({x: 3, y: 3})).toBeFalsy();
-  expect(gameboardController.receiveAttack(3,3)).toBeFalsy();
+  expect(gameboardController.receiveAttack(3,3)).toMatchObject(validAttackWater);
   expect(gameboardController.trackAttacks.has({x: 3, y: 3})).toBeTruthy();
 });
 
 it('receive an attack and hit', () => {
   expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeFalsy();
-  expect(gameboardController.receiveAttack(0,0)).toBeTruthy();
+  expect(gameboardController.receiveAttack(0,0)).toMatchObject(validAttackHit);
   expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeTruthy();
 });
 
+it('receive attacks and sunk', () => {
+  expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeFalsy();
+  expect(gameboardController.trackAttacks.has({x: 0, y: 1})).toBeFalsy();
+  expect(gameboardController.receiveAttack(0,0)).toMatchObject(validAttackHit);
+  expect(gameboardController.receiveAttack(0,1)).toMatchObject(validAttackSunk);
+  expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeTruthy();
+  expect(gameboardController.trackAttacks.has({x: 0, y: 1})).toBeTruthy();
+});
+
+it('receive attacks and sunk #strings', () => {
+  expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeFalsy();
+  expect(gameboardController.trackAttacks.has({x: 0, y: 1})).toBeFalsy();
+  expect(gameboardController.receiveAttack('0','0')).toMatchObject(validAttackHit);
+  expect(gameboardController.receiveAttack('0','1')).toMatchObject(validAttackSunk);
+  expect(gameboardController.trackAttacks.has({x: 0, y: 0})).toBeTruthy();
+  expect(gameboardController.trackAttacks.has({x: 0, y: 1})).toBeTruthy();
+});
+
 it('invalid receive attack location #1', () => {
-  expect(gameboardController.receiveAttack(-1, 0)).toBeFalsy();
+  expect(gameboardController.receiveAttack(-1, 0)).toMatchObject(invalidAttack);
 });
 
 it('invalid receive attack location #2', () => {
-  expect(gameboardController.receiveAttack(-1, 0)).toBeFalsy();
+  expect(gameboardController.receiveAttack(-1, 0)).toMatchObject(invalidAttack);
 });
 
 it('invalid receive attack location #3', () => {
-  expect(gameboardController.receiveAttack('ciao', 4)).toBeFalsy();
+  expect(gameboardController.receiveAttack('ciao', 4)).toMatchObject(invalidAttack);
 });
 
 it('invalid receive attack location #4', () => {
-  expect(gameboardController.receiveAttack(0, 15)).toBeFalsy();
+  expect(gameboardController.receiveAttack(0, 15)).toMatchObject(invalidAttack);
 });
+
